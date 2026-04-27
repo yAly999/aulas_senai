cac97ed607c87902990bd7cdcd381c5bb933e1ad
yAly999 <alysson.delpawlack@gmail.com>
2026-04-27 16:01:08 -0300

    ...

--- funcoes.py
+++ funcoes.py
@@ -7,4 +7,5 @@ n1 = float(input("digite a primeira nota do aluno: "))
 n2 = float(input("digite a segunda nota do aluno: "))
 n3 = float(input("digite a terceira nota do aluno: "))
 
-resultado = calcular_media(n1,n2,n3)
+resultado = calcular_media(n1, n2, n3)
+print(f"A media final é: {resultado}")
