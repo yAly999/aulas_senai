c6ad234f34f29c195828af160984324c47522def
yAly999 <alysson.delpawlack@gmail.com>
2026-04-27 16:40:00 -0300

    bla

--- minha_funcao.py
+++ minha_funcao.py
@@ -1,9 +1,17 @@
-globa = "essa variavel recebe nim qualquer lugar"
+# globa = "essa variavel recebe nim qualquer lugar"
 
-def minha_funcao():
-    print("Essa é minha função")
-    local = "esssa variavel so pode ser utilizada na função"
-    print(globa)
+# def minha_funcao():
+#     print("Essa é minha função")
+#     local = "esssa variavel so pode ser utilizada na função"
+#     print(globa)
 
-print(globa)
-minha_funcao()    
+# print(globa)
+# minha_funcao()    
+
+nome = "aly"
+
+def saudacao():
+    sobrenome = "code"
+    print(f"Olá, {nome} {sobrenome}")
+
+saudacao()
