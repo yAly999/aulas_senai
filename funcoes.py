06ed6b0c61df2532fdd9521a1e23d8461be22639
yAly999 <alysson.delpawlack@gmail.com>
2026-04-27 16:04:17 -0300

    fix

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,7 @@
 def calcular_media(nota1, nota2, nota3):
     soma = nota1 + nota2 + nota3
     media = soma/3
-    return
+    return media
 
 n1 = float(input("digite a primeira nota do aluno: "))
 n2 = float(input("digite a segunda nota do aluno: "))
