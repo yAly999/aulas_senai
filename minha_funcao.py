6ec85c2e06489c6e02e66ca1048a47a89a150820
yAly999 <alysson.delpawlack@gmail.com>
2026-04-27 16:34:52 -0300

    ais veis

--- minha_funcao.py
+++ minha_funcao.py
@@ -1,7 +1,9 @@
+globa = "essa variavel recebe nim qualquer lugar"
+
 def minha_funcao():
     print("Essa é minha função")
     local = "esssa variavel so pode ser utilizada na função"
+    print(globa)
 
-
+print(globa)
 minha_funcao()    
-print(local)
