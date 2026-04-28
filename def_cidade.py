d459e33254270021cfa69ec5fb87c0dc645afa7b
yAly999 <alysson.delpawlack@gmail.com>
2026-04-28 15:49:13 -0300

    .

--- def_cidade.py
+++ def_cidade.py
@@ -3,5 +3,10 @@ def criar_perfil(nome, idade, cidade):
 
 criar_perfil(nome = "alysson", idade = 18, cidade = "curitiba" )
 
+def somar_tudo(*numeros):
+    return sum(numeros)
 
+somar_tudo(1, 2)
+somar_tudo(1, 2, 3)
+somar_tudo(1, 2, 5, 6)
 
